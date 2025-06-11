c943372f3fda6cd38dd04248e2a1e4658c30e20e
sulfikaralijun <sulfikaralijun@gmail.com>
2025-06-11 18:11:20 +0800

    update text message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json({ message: "Hello Devops" });
 });
 
 app.listen(PORT, () => {
